a9635a5e445c837fa7f7d53d0a542b873d64fb81
Marta Żak <martysik19@gmail.com>
2024-09-03 23:46:02 +0200

    Adds one more column to the table

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write your code here
+const refs = {
+  table: document.querySelector('table'),
+};
+
+const tableRows = refs.table.rows;
+
+for (const row of tableRows) {
+  for (let i = 0; i < row.children.length; i++) {
+    if (i === 1) {
+      const newTableData =
+        row.parentElement.tagName === 'THEAD' ||
+        row.parentElement.tagName === 'TFOOT'
+          ? document.createElement('th')
+          : document.createElement('td');
+
+      newTableData.textContent = row.children[i].textContent;
+      row.insertBefore(newTableData, row.lastElementChild);
+    }
+  }
+}
